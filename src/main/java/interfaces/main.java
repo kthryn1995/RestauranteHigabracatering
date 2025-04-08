1a20fd444bb5a16da5ef8562ad14bc5f02a3f1b3
kthryn1995 <kthryntobon@hotmail.com>
2025-04-07 23:35:51 -0500

    se modifica metodo para añadir pedidos

---
+++ src/main/java/interfaces/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package interfaces;
+
+/**
+ *
+ * @author Kthryn
+ */
+public class main {
+
+        public static void main(String[] args) {
+            
+      
+    }
+}
